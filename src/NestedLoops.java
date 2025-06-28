7345101da475ff43b7680de0754f573388126ae3
Your Name <your.email@example.com>
2025-06-28 13:44:37 -0400

    Final Commit

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+public class NestedLoops
+{
+    public static void main(String[] args) {
+        System.out.println("Task 7: ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("Task 5: ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("Task 6: ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j-=1) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
-public class NestedLoops {
+
+    }
 }
+
